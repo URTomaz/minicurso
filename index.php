d8a7f6acf461a8c655738f7c3a4565cc3be13259
UlissesRTomaz <ulisses.rtomaz@gmail.com>
2020-11-27 10:47:12 -0300

    Configurações da página principal

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minicurso Git e GitHub</title>
+</head>
+<body>
+    <h1>Título desta página</h1>
+</body>
+</html>
